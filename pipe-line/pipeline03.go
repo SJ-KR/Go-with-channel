4d026bfc4a54f6ee456f84b92c3d18d4c4f63302
SJ-KR <djtm99@naver.com>
2020-06-14 15:06:35 -0700

    add pipeline examples

---
+++ pipe-line/pipeline03.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func gen(done chan struct{}, nums ...int) <-chan int {
+	out := make(chan int, len(nums))
+
+	go func() {
+		defer close(out)
+		for _, n := range nums {
+			select {
+			case out <- n:
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out
+}
+
+func sq(done <-chan struct{}, in <-chan int) <-chan int {
+	out := make(chan int)
+
+	go func() {
+		defer close(out)
+		for n := range in {
+			select {
+			case out <- n * n:
+			case <-done:
+				return
+			}
+		}
+	}()
+	return out
+}
+
+func merge(done chan struct{}, cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	output := func(c <-chan int) {
+		for n := range c {
+			select {
+			case out <- n:
+			case <-done:
+				return
+			}
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+func main() {
+	done := make(chan struct{})
+	defer close(done)
+
+	in := gen(done, 2, 3, 5, 7, 11, 13)
+
+	c1 := sq(done, in)
+	c2 := sq(done, in)
+
+	for n := range merge(done, c1, c2) {
+		fmt.Println(n)
+	}
+}
